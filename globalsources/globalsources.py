d5226b5144e75ab717cecc953f58e1fd4c414e0a
taha hamedani <taha_hamedani@yahoo.com>
2019-06-10 16:28:43 +0430

    add category links of globalresources

--- globalsources/globalsources.py
+++ globalsources/globalsources.py
@@ -1,42 +1,22 @@
-import requests
-from bs4 import BeautifulSoup
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy.crawler import CrawlerProcess
 import json
 import urllib
 import multiprocessing
-###########################################################
-###########################################################
+import re
+import requests
+from bs4 import BeautifulSoup
+import urlparse3
+
+from pymongo import MongoClient
+##################################################
+##################################################
 headers = {
-    'User-Agent': ''
+    'User-Agent': 'Mozilla\/5.0 (compatible MSIE 10.0 Windows Phone 8.0 Trident\/6.0 IEMobile\/10.0 ARM Touch NOKIA Lumia 520)',
+    'referer': ''
 }
-manager = multiprocessing.Manager()
-final_list = manager.list()
-###########################################################
-###########################################################
-def chunkIt(seq, num):
-    avg = len(seq) / float(num)
-    out = []
-    last = 0.0
-
-    while last < len(seq):
-        out.append(seq[int(last):int(last + avg)])
-        last += avg
-
-    return out
-###########################################################
-def create_category_url():
-    '''
-    function_name: create_category_url
-    input: none
-    output: start_urls for scrapy
-    description: add products to urls from products_go4w.json file
-    '''
-    with open('CategoriesLinks_gr.json') as f:
-        products = json.load(f)
-    urls = []
-    for p in list(products):
-        urls.append(str(p['name']))
-    return urls
-###########################################################
+##################################################
 def change_proxy():
     '''
     function_name: change_proxy
@@ -52,87 +32,86 @@ def change_proxy():
 
     resp = requests.get(url=url, params=params)
     data = json.loads(resp.text)
-
+    print('********************************************')
+    print('Changing Proxy ... ' + data['proxy'])
+    print('********************************************')
     return data['proxy'], data['randomUserAgent']
-################################################################
-def get_pages(index, urls):
 
-    proxy, useragent = change_proxy()
+###########################################################
+def create_category_url():
+    '''
+    function_name: create_category_url
+    input: none
+    output: start_urls for scrapy
+    description: add products to urls from products_alibaba.json file
+    '''
 
-    headers = {
-        'User-Agent': 'Mozilla\/5.0 (compatible MSIE 10.0 Windows Phone 8.0 Trident\/6.0 IEMobile\/10.0 ARM Touch NOKIA Lumia 520)',
-    }
-
-    proxy = "101.51.141.123:8080"
+    #proxy, useragent = change_proxy()
+    from selenium import webdriver
+    driver = webdriver.chrome()
+    driver.get("https://www.globalsources.com/")
+    commentHTML = driver.find_elements_by_class_name('item')
 
     #headers['User-Agent'] = useragent
-    cnt = 0
+    soup = BeautifulSoup(requests.get("https://www.globalsources.com/").content, 'html.parser')
+    lis = soup.findAll("a")
+
+    for tag in lis:
+        for subtag in tag.descendants:
+            print(subtag)
+
+
+    urls = [li.find('a') for li in lis if li]
+    urls = [url for url in urls if url is not None]
+    urls = [url.attrs['href'] for url in urls if 'pid' in str(url)]
+
+    return urls
+############################################################
+############################################################
+def product_parse(urls):
+    '''
+    function_name: product_parse
+    input: url
+    output: none
+    description: crawl product page
+    '''
+
+###########################################################
+###########################################################
+def main_parse(urls):
+    '''
+    function_name: main_parse
+    input: list
+    output: none
+    description: first level of crawling
+    '''
+    proxy, useragent = change_proxy()
+    headers['User-Agent'] = useragent
+    ########################################################
     for url in urls:
-        print('AAAAAAAAAAAAAAAAAAAAAAAAA')
+        # Products
+        for i in range(1, 101):
+            try:
+                soup = BeautifulSoup(requests.get(str(url) + "?spm=a2700.galleryofferlist.pagination&page=" + str(i), proxies={'http': proxy}, headers=headers).content, 'html.parser')
+                items = soup.find_all('h2', class_='title')
+                items_urls = [i.find('a').attrs['href'] for i in items]
 
-        # try:
-        #     soup = BeautifulSoup(requests.get(url).content, 'html.parser')
-        #     lis = soup.select("span[class=nonLink]").text
-        #     if(len(lis)>0):
-        #         tokens = str(lis[len(lis)-1].get('href')).split('&')
-        #         for t in tokens:
-        #             if "pg_" in str(t):
-        #                 page = int(t.split('=')[-1])
-        #                 final_list.append(page)
-        #                 break
-        # except urllib.error.HTTPError as e:
-        #     if(e.code == 403 and False):
-        #         proxy, useragent = change_proxy()
-        #         headers['User-Agent'] = useragent
-        #         print('************************************')
-        #         print('Changing Proxy...')
-        #         print('************************************')
-        #
-        # cnt = cnt + 1
-        # print('Process ' + str(index+1) + ': ' + str(cnt) + ' has been done from ' + str(len(urls)))
-########################################################################
+                for iu in items_urls:
+                    product_parse(iu)
 
-def getAllCategoryLinks():
+            except urllib.error.HTTPError as e:
+                if (e.code == 403):
+                    proxy, useragent = change_proxy()
+                    headers['User-Agent'] = useragent
+                    print('********************************************')
+                    print('Changing Proxy ... ' + proxy)
+                    print('********************************************')
+            except:
+                pass
 
-    categoriesLinkFile = open("CategoriesLinks_gr.txt", "w")
+############################################################
+############################################################
+############################################################
 
-    soup = BeautifulSoup(requests.get('https://www.globalsources.com/').content, 'html.parser')
-    #lis = soup.select("ul[class=list]")
-    dt_data = soup.find_all("li", {"class": "item"})    #soup.find('a').text
-    categoriesLinkFile.close()
-
-urls = ['https://www.globalsources.com/'] #create_category_url()
-getAllCategoryLinks()
 urls = create_category_url()
-
-parts = chunkIt(urls, 5)
-
-processes = []
-
-for i in [0,1,2,3,4]:
-    processes.append(multiprocessing.Process(target=get_pages, args=[i,parts[i]]))
-
-
-for p in processes:
-    p.start()
-
-for p in processes:
-    p.join()
-
-
-total_pages = 0
-for s in final_list:
-    total_pages = total_pages + int(s)
-
-print("Total Page = " + str(total_pages))
-
-
-
-
-
-
-
-
-
-
-
+main_parse(urls)
